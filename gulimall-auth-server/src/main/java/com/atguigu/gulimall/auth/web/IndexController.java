ae176c9209117557a698d30fe984101eee7131f3
tifa <tifa@139.com>
2025-10-13 21:29:38 +0800

    feat:新增了注册和登录页面，处理好了静态资源。

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.atguigu.gulimall.auth.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+  @GetMapping("/regist")
+  public String register() {
+    return "reg";
+  }
+}
